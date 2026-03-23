79eb290bb7042ba7fdddd94d34bc5417cda56880
Belgos22 <emmanuelsrgbelgos@gmail.com>
2026-03-23 12:04:15 +0100

    feat : connexion PostgreSQL avec TypeORM et entité Todo

--- backend/src/todos/todos.service.ts
+++ backend/src/todos/todos.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from './todo.entity';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  constructor(
+    @InjectRepository(Todo)
+    private readonly todosRepository: Repository<Todo>,
+  ) {}
+
+  findAll(): Promise<Todo[]> {
+    return this.todosRepository.find();
+  }
+
+  create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    const todo = this.todosRepository.create(createTodoDto);
+    return this.todosRepository.save(todo);
+  }
+
+  async update(id: number, done: boolean): Promise<Todo | null> {
+    await this.todosRepository.update(id, { done });
+    return this.todosRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.todosRepository.delete(id);
+  }
+}
